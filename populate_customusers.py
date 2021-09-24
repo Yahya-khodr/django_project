4905e9058ffc344b91eaccbbb1ed20e1c988b535
Yahya khodr <yhy97kh18@gmail.com>
2021-09-24 18:46:20 +0300

    faker email

--- populate_customusers.py
+++ populate_customusers.py
@@ -13,7 +13,8 @@ def username(num_of_name=10):
     for i in range(num_of_name):
         faker_fname = fake.first_name()
         faker_lname=fake.last_name()
-        name= customUser.objects.create(firstName=faker_fname,lastName=faker_lname)
+        faker_email = fake.ascii_free_email()
+        name= customUser.objects.create(firstName=faker_fname,lastName=faker_lname,email=faker_email)
         name.save()
         
 
